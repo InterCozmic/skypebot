764462fb44c0188d2bdb2b44156b4a888e519874
InterCozmic <66198025+InterCozmic@users.noreply.github.com>
2021-10-13 17:00:28 +1100

    Fix typo in setup.py
    Oops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='skypebot',
     author='InterCozmic',
-    description='A module made for coding text command bots for Skype, with Python.'
+    description='A module made for coding text command bots for Skype, with Python.',
     version='0.1.0',
     packages=find_packages()
 )
