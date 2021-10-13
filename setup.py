d6c553f7f43cb447b97ae66010aac0e4ce7f08a4
InterCozmic <66198025+InterCozmic@users.noreply.github.com>
2021-10-13 17:10:43 +1100

    Update setup.py
    Dependencies added

--- setup.py
+++ setup.py
@@ -5,5 +5,16 @@ setup(
     author='InterCozmic',
     description='A module made for coding text command bots for Skype, with Python.',
     version='0.1.0',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4>=4.10.0',
+        'certifi>=2021.5.30',
+        'charset-normalizer>=2.0.6',
+        'idna>=3.2',
+        'requests>=2.26.0',
+        'SkPy>=0.10.4',
+        'soupsieve>=2.2.1',
+        'urllib3>=1.26.7',
+        'wincertstore>=0.2',
+    ]
 )
