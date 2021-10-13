dcfcab0ae91793709d60dec05e285ef4cf4de222
InterCozmic <66198025+InterCozmic@users.noreply.github.com>
2021-10-13 16:59:54 +1100

    Create setup.py
    I don't know if this will work

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skypebot',
+    author='InterCozmic',
+    description='A module made for coding text command bots for Skype, with Python.'
+    version='0.1.0',
+    packages=find_packages()
+)
