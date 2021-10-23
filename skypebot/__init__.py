b74befb7dfd0e77b7dd1f7ef5b0998383132e448
InterCozmic <66198025+InterCozmic@users.noreply.github.com>
2021-10-23 15:02:16 +1100

    Version 0.1.3
    It turns out I can use __main__.py to do quickstarty things

--- skypebot/__init__.py
+++ skypebot/__init__.py
@@ -63,31 +63,3 @@ def login(commands, email, password):
   skype = SkypePing(user=email, pwd=password, autoAck=True)
   skype.initialise(commands.commands, commands.handlers, commands.prefix)
   skype.loop()
-
-if __name__ == "__main__":
-  #Example Code
-  #If you use this in your bot, make sure to un-comment the following line to import the module:
-  #from skypebot import *
-
-  email = input('Email: ')
-  password = input('Password: ')
-  message = Message('!') #initialise message with the prefix '!'
-
-  #example of a message handler
-  @message.handler() #custom handler
-  def messagehandle(message):
-    print(str(message.user.name)+': '+message.content) #print the message's author and content
-
-  #example of a basic command
-  @message.command('hi') #hi command - can be accessed in Skype by typing '!hi'
-  def hi(message, arguments):
-    message.chat.sendMsg('hello') #send 'hello' back.
-
-  #example of a command with arguments
-  @message.command('repeat', ['times', 'thing']) #repeat command - can be accessed in Skype by typing '!repeat <number_of_times> <thing_to_repeat>'
-  def repeater(message, arguments):
-    string = arguments.thing + '\n' #create a string with the thing to repeat
-    string = string * int(arguments.times) #repeat that thing multiple times
-    message.chat.sendMsg(string) #send to chat
-
-  login(message, email, password) #log in to the bot
